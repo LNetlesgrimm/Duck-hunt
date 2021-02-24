7cfc9ed3af72bfef21d2580a0491812b7047ab4b
LNetlesgrimm <kohler.helene@gmail.com>
2021-02-24 11:37:01 +0100

    keyboard

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 const playerKeyboard = document.querySelector('#cube');
-const top = 10;
-const left = 10;
+document.addEventListener('keydown', moveCube);
+function moveCube(event) {
+    const key = event.code;
+    console.log(key);
+    const aDown = key == 'ArrowDown';
+    const aUp = key == 'ArrowUp';
+    const aLeft = key == 'ArrowLeft';
+    const aRight = key == 'ArrowRight';
+    let posTop = 10;
+    let posLeft = 10;
+    switch (key) {
+        case aDown:
+            playerKeyboard.style.top = posTop + '50%';
+            posTop++;
+            break;
+            case aUp:
+               
+                break;
+                case aRight:
+                    
+                    break;
+                    case aLeft:
+                        
+                        break;
+                        
+                        default:
+                            break;
+    }
+}
