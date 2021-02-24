eb3067ba2c6b9ba057a0739bbe1f6cae951ee4e7
Ivan <ivan@register.me>
2021-02-24 11:36:42 +0100

    function touch

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-const playerKeyboard = document.querySelector('#cube');
-const top = 10;
-const left = 10;
+
+
+const playerkeyboard = document.querySelector('#cube');
+
+
+playerkeyboard.addEventListener('click', touched)
+
+
+
+function touched(event) {
+
+    //chanch the status od 
+    alert('You Win!');//debugger
+    playerkeyboard.classList.add('cube_shuted');
+    //stop the event listenet of the mouse and keyboard
+    playerkeyboard.removeEventListener('click', touched);
+    playerkeyboard.removeEventListener('keydown', moveCube);
+
+
+}
+
+
+
